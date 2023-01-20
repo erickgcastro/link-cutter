dbd0c67bd5a1e05f3189549ecfb8cf134424afd2
Erick Gabriel <limaerick.03@gmail.com>
2023-01-20 13:05:11 -0300

    sda

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6912083168339143'
+          crossOrigin='anonymous'
+        ></script>
         <link rel='icon' type='image/png' sizes='32x32' href='/images/icon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/images/icon-16x16.png' />
         <meta name='twitter:card' content='summary'></meta>
